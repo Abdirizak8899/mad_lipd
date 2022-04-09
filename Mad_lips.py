2f8b2df7ee5ea821548d887c0231b2744fbcfa27
Andirizak abdullahi hussein <102373033+Abdirizak8899@users.noreply.github.com>
2022-04-09 14:17:00 +0300

    Add files via upload

---
+++ Mad_lips.py
@@ -0,0 +1,24 @@
+# copy right of this code have Abdirizak abdullahi hussein 
+print('Welcome!')
+print()
+
+Name = input("What's your name?  ")
+
+print('Nice to meet you', Name)
+
+print()
+
+Age = input('How old are you? ')
+if Age < '17':
+	print('You are younger than me cuz i am 17')
+elif Age == '17':
+	print('We are the same age ')
+else:
+	print('You are older than me cuz am 17')
+print()
+Nash = input("What's your background")
+
+print('I love ' + Nash)
+
+print()
+print('Good bye !' + Name)
